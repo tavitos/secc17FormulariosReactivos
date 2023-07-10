193e9b6707ecc536927220590344aa4bece9123e
tavitos <lesamaoyabi@gmail.com>
2023-07-10 17:44:43 -0600

    Sección 17 Formularios reactivos hasta 232.

---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './switches-page.component.html',
+  styles: [
+  ]
+})
+export class SwitchesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+  ]
+})
+export class SideMenuComponent implements OnInit {
+
+  public reactiveMenu: MenuItem[] = [
+    { title: 'Básicos', route: './reactive/basic'},
+    { title: 'Dinámicos', route: './reactive/dynamic'},
+    { title: 'Switches', route: './reactive/switches'}
+  ];
+  public authMenu: MenuItem[] = [
+    { title: 'Registro', route: './auth/'}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
