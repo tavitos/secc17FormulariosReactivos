8bdcf1862e08271197d3b31268db01a735b034d2
tavitos <lesamaoyabi@gmail.com>
2023-07-28 18:18:36 -0600

    Finiquitado sección 18 Validaciones curso angular Fernando Herrera.

--- src/app/shared/service/validators.service.ts
+++ src/app/shared/service/validators.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({providedIn: 'root'})
 export class ValidatorsService {
@@ -24,5 +24,25 @@ export class ValidatorsService {
 
     public isValidField( form: FormGroup, field: string ){
         return form.controls[field].errors && form.controls[field].touched;
+    }
+
+    public isFieldOneEqualFieldTwo(field1: string, field2: string){
+        return (formGroup: AbstractControl): ValidationErrors|null => { /** Función utilizada para evaluar, conexión con el formulario
+                                                                      marca deprecated en .group del register-page línea 15
+                                                                      porque el tipo debe ser en lugar de FormGroup debe ser
+                                                                      AbstractControl<any, any>*/
+            const fieldValue1 = formGroup.get(field1)?.value;
+            const fieldValue2 = formGroup.get(field2)?.value;
+
+            if (fieldValue1 !== fieldValue2) {
+                formGroup.get(field2)?.setErrors({notEqual: true});
+                return { notEqual: true } /** Validación del error del formulario */
+            }
+            
+            formGroup.get(field2)?.setErrors(null);
+
+            return null;
+
+        }
     }
 }
